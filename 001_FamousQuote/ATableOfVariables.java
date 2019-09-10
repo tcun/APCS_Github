0904870235cf161e1ef3f10adc8b8c3a88e1da1f
Moof <47290472+Moo-f@users.noreply.github.com>
2019-09-09 20:43:34 -0500

    Idk How
    I can only get it to run when the java files are in 001_FamousQuote

--- 001_FamousQuote/ATableOfVariables.java
+++ 001_FamousQuote/ATableOfVariables.java
@@ -3,19 +3,22 @@ public class ATableOfVariables
     //default main method
    public static void main(String args[])
    {
+//method printing out the numbers in table
        integerPrint();
    }
 
     //method being called in main
     public static void integerPrint()
     {
+        int variable;
+
         variable = 1;
     //print variable table
-        System.out.println("a" + "a^2" + "a^3");
-        System.out.println(variable+variable + " " + squaredOperator(variable)+ " " + cubedOperator(variable));
-        System.out.println(variable*2 + " " + squaredOperator(variable*2)+ " " + cubedOperator(variable*2));
-        System.out.println(variable*3 + " " + squaredOperator(variable*3) + " " + cubedOperator(variable*3));
-        System.out.println(variable*4 + " " + squaredOperator(variable*4)+ " " + cubedOperator(variable*3));
+        System.out.println("a" + " " + "a^2" + " "  + "a^3");
+        System.out.println(variable + "  " + squaredOperator(variable)+ "  " + cubedOperator(variable));
+        System.out.println(variable*2 + "  " + squaredOperator(variable*2)+ "  " + cubedOperator(variable*2));
+        System.out.println(variable*3 + "  " + squaredOperator(variable*3) + "  " + cubedOperator(variable*3));
+        System.out.println(variable*4 + "  " + squaredOperator(variable*4)+ " " + cubedOperator(variable*4));
     }
 // https://stackoverflow.com/questions/22084373/does-java-have-an-exponential-operator
 // I was "inspired" by this code
@@ -28,6 +31,7 @@ public class ATableOfVariables
         nSquared = n*n;
         return (nSquared);
     }
+// takes an inputted int and multiplies it by itself twice
     public static int cubedOperator(int n)
     {
         int nCubed;
