be159445474df556036d242a7b1ff9a93875c2f1
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-09-13 09:34:02 -0500

    small edits

--- Java Projects/SummationOfASeries.java
+++ Java Projects/SummationOfASeries.java
@@ -1,9 +1,7 @@
-public class SummationOfASeries
-{
-    //calling main method that runs main program
-    public static void main(String args[])
-    {
-    //int declarations
+public class SummationOfASeries {
+    // calling main method that runs main program
+    public static void main(String args[]) {
+        // int declarations
         int one;
         int two;
         int three;
@@ -15,21 +13,20 @@ public class SummationOfASeries
         int nine;
 
         int total;
-    //setting values to int's
+        // setting values to int's
         one = 1;
         two = 2;
         three = 3;
         four = 4;
         five = 5;
         six = 6;
-        seven = 7;        
-        eight = 8;    
+        seven = 7;
+        eight = 8;
         nine = 9;
 
-    //adding ints together and setting them to total
         total = one + two + three + four + five + six + seven + eight + nine;
 
-    //print total
+        // print total
         System.out.println(total);
     }
 }
