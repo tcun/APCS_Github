8e01c67a24c5123550f59e7524e78dc1b77e7952
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-09-13 10:20:47 -0500

    AreaOfRactangles && Compute
    Need Comments

---
+++ Java Projects/AreasOfRectangles.java
@@ -0,0 +1,42 @@
+public class AreasOfRectangles
+{
+    public static void main(String[] args)
+    {
+        System.out.println(areaFormula(5.5, 7.8));
+        System.out.println(areaFormula(99.4, 500.352));
+        System.out.println(areaFormula(inchToCent(75), 800));
+        System.out.println(areaFormula(500029.75, centToInch(90029384.0)));
+        System.out.println(areaFormula(centToInch(600000.0), milliToYard(8343491034.0)));
+    }
+    public static double areaFormula(double l, double w)
+    {
+        double area;
+        area = l*w;
+        return area;
+        
+    }
+
+    public static double inchToCent(double inch)
+    {
+        double cent = inch / 0.393701;
+        return cent;
+    }
+
+    public static double centToInch(double cent)
+    {
+        double inch = cent / 2.54;
+        return inch;
+    }
+
+    public static double centToYard(double cent)
+    {
+        double yard = cent / 0.0109361;
+        return yard;
+    }
+
+    public static double milliToYard(double milli)
+    {
+        double yard = milli / 0.00109361;
+        return yard;
+    }
+}
---
+++ Java Projects/ComputeAnExpression.java
@@ -0,0 +1,8 @@
+public class ComputeAnExpression
+{
+    public static void main(String[] args)
+    {
+        double total = ((9.5*4.5) - (92.5 / 3))/(45.5-3.5);
+        System.out.println(total);
+    }
+}
