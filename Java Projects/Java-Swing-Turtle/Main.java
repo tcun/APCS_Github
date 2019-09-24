e0a3428ab05559e28ebd7680802a2c8a3582ba16
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-09-24 10:08:32 -0500

    Turtle Class Import & other things
    SweetGarage.java
    Turtle > Main.java

---
+++ Java Projects/Java-Swing-Turtle/Main.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+import java.awt.*;
+
+public class Main {
+  public static void main(String[] args) {
+    //Refer back to your Runestone academy book as needed.
+    //Instantiate the world. The world should be 220x220.
+    
+    //Instantiate a turtle. Name it as you wish.
+
+    /*Have your turtle make 4 boxes in the world. The boxes should be in a 2x2 grid. Similar to this:
+     ___ ___ 
+    |   |   |
+    |___|___|
+    |   |   |
+    |___|___|
+
+    */
+
+
+    
+    //Finally, set the world to be visible with a boolean...
+
+    World world = new World(500,500);
+  }
+}
---
+++ Java Projects/SweetGarage.java
@@ -0,0 +1,13 @@
+public class SweetGarage
+{
+    public static main(String args[])
+    {
+        Car defaultCar = new Car();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        Car car4 = new Car();
+
+        System.out.println(defaultCar);
+    }
+}
