aa198a96b3bb8eb1c23e9c56bf90306220645c50
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-10-22 09:57:40 -0500

    set up
    sdaf

---
+++ 021_FlipACoin /FlipACoin.java
@@ -0,0 +1,7 @@
+public class FlipACoin
+{
+    public static void main(String args[])
+    {
+        
+    }
+}
---
+++ 023_BlackjackOne/BlackjackOne.java
@@ -0,0 +1,7 @@
+public class BlackjackOne
+{
+    public static void main(String args[])
+    {
+        
+    }
+}
