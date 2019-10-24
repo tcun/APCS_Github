aa02dfb06a19ad1765763131dafd167878cbdb03
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-10-24 10:20:14 -0500

    why wont check work

--- 022_IsItLegalForYou/IsItLegalForYou.java
+++ 022_IsItLegalForYou/IsItLegalForYou.java
@@ -7,18 +7,22 @@ public class IsItLegalForYou
     {
         Scanner scan = new Scanner(System.in);
         int num;
+        //intro and asking for input through scanner
         System.out.println("Can you get a Driver's licence, buy cigarettes, go to a casino, or rent a car?");
         System.out.println("Input your age:");
         num = scan.nextInt();
         
-        if(num < 17)
+        //if below 17 just get out no childs allowed
+        if(num =< 17)
         {
             System.out.println("Begone child");
         }
+        //if between 18 and 20, you do everything but buy alcohol and rent a car
         else if(num >= 18 && num <= 20 )
         {
             System.out.println("You can drive, go to the casino, and buy cigarrettes (just not in Minneapolis), but you cannot buy alcohol or rent a car.");
         }
+        //if between 21 and 24 they do everything but rent a car
         else if(num >= 21 && num <= 24 )
         {
             System.out.println("You can drive, go to the casino, buy alcohol, and buy cigarrettes anywhere, but you cannot rent a car.");
@@ -27,6 +31,7 @@ public class IsItLegalForYou
         // {
         //     throw new InputMismatchException("Don't be difficult");
         // }
+        //if above  or equal to 25 you're all good 
         else
         {
             System.out.println("You can do it all baby, you are a fully-fledged adult");
--- 023_BlackjackOne/BlackjackOne.java
+++ 023_BlackjackOne/BlackjackOne.java
@@ -1,50 +1,56 @@
 import java.util.Scanner;
-public class BlackjackOne
-{
-    public static int total;
 
-    public static void main(String args[])
-    {
+public class BlackjackOne {
+    public static int total;
+    public static String check;
+
+    public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
+        // setting a random num to card1
         int card1 = deal();
         System.out.println("You got a " + card1);
+        // setting a random num to card1
         int card2 = deal();
         System.out.println("You got a " + card2);
+        // prints total of the two cards
         total = card1 + card2;
         System.out.println("Your total is: " + total);
-
-        System.out.println("Hit or Stay?");
-        String check = scan.next();
-
-        if(total < 21)
-        {
-             if(check == "hit" || check == "Hit")
-             {
-                int card3 = deal();
-                System.out.println("You got a" + card3);
-                 total += card3;
-                System.out.println("Your total is: " + total);
-             }
-            else
-            {
-                System.out.println("Well you don't have an opponent, so I don't know if you won.");
-                scan.close();
-            }
         
+        // primary check to see if total is below 21
+        if (total < 21) {
+            // scanning next line for user response
+            System.out.println("Hit or Stay?");
+            check = scan.nextLine();
+            scan.close();
+            // checking if the String check is equal to hit
+            if (check.toLowerCase() == "hit") {
+
+                // adds another card to total
+                int card3 = deal();
+                System.out.println("You got a " + card3);
+                total += card3;
+                System.out.println("Your total is: " + total);
+            }
+            // if anything else it will end the game
+            else {
+                System.out.println("Well you don't have an opponent, so I don't know if you won.");
+                
+            }
         }
-    
-        else if(total == 21)
-        {
+        // primary check to see if total is equal to 21
+        else if (total == 21) {
             System.out.println("You for sure won, even without an opponent!");
         }
-        else
-        {
+        // if it's over 21 you lose
+        else {
             System.out.println("You automatically lose, RIP.");
         }
-}
-    public static int deal()
-    {
-        double num = Math.random() * 10 + 1;
-        return (int)num;
     }
+
+    // generates a number between 1-10
+    public static int deal() {
+        double num = Math.random() * 10 + 1;
+        return (int) num;
+    }
+
 }
