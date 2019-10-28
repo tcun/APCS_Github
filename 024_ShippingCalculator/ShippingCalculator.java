bdcb1f522803c673ee61bba8ff202c13b72f3657
CDHRaiderBots <raiderbots@c-dh.org>
2019-10-28 16:26:38 -0500

    wow biggum

--- 024_ShippingCalculator/ShippingCalculator.java
+++ 024_ShippingCalculator/ShippingCalculator.java
@@ -1,26 +1,35 @@
 import java.util.Scanner;
 
 public class ShippingCalculator {
+    public static double w;
     public static void main(String args[]) {
-        Scanner scan = new Scanner();
-        w = scan.next();
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input valid weight: ");
+        w = scan.nextDouble();
+        System.out.println(check());
+        scan.close();
     }
 
-    public static int check()
+    public static double check()
     {
-        if(0 < w && w < 3  ){
+        if(0 < w && w < 1  ){
             return 3.5;
         }
-        else if(){
-
+        else if(1 < w && w <= 3){
+            return 5.5;
         }
-        else if(){
-
+        else if(3 < w && w <= 10){
+            return 8.5;
         }
-        else if(){
-
+        else if(10 < w && w <= 20){
+            return 10.5;
         }
         else
+        {
+            System.out.println("Invalid Input");
+            return 0;
+        
+        }
     }
 }
+
