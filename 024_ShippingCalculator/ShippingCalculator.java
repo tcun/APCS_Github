aca4fee8725fd2f0776ed60b8fdebb6aec5fc95c
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-10-29 09:58:44 -0500

    Revert "wow biggum"
    This reverts commit bdcb1f522803c673ee61bba8ff202c13b72f3657.

--- 024_ShippingCalculator/ShippingCalculator.java
+++ 024_ShippingCalculator/ShippingCalculator.java
@@ -1,35 +1,26 @@
 import java.util.Scanner;
 
 public class ShippingCalculator {
-    public static double w;
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Input valid weight: ");
-        w = scan.nextDouble();
-        System.out.println(check());
-        scan.close();
+        Scanner scan = new Scanner();
+        w = scan.next();
+
     }
 
-    public static double check()
+    public static int check()
     {
-        if(0 < w && w < 1  ){
+        if(0 < w && w < 3  ){
             return 3.5;
         }
-        else if(1 < w && w <= 3){
-            return 5.5;
+        else if(){
+
         }
-        else if(3 < w && w <= 10){
-            return 8.5;
+        else if(){
+
         }
-        else if(10 < w && w <= 20){
-            return 10.5;
+        else if(){
+
         }
         else
-        {
-            System.out.println("Invalid Input");
-            return 0;
-        
-        }
     }
 }
-
