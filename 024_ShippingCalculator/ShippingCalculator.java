d914fcc0b27f432aad9613cfa9cac349b24d7daa
ThomasRC-bit <54677598+ThomasRC-bit@users.noreply.github.com>
2019-10-29 21:58:36 -0500

    shippingcalc please work

--- 024_ShippingCalculator/ShippingCalculator.java
+++ 024_ShippingCalculator/ShippingCalculator.java
@@ -2,34 +2,33 @@ import java.util.Scanner;
 
 public class ShippingCalculator {
     public static double w;
+
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Input valid weight: ");
+        //assign w to scan the next double input through terminal
         w = scan.nextDouble();
+        //prints the check function
         System.out.println(check());
         scan.close();
     }
 
-    public static double check()
-    {
-        if(0 < w && w < 1  ){
+    //check function that returns a double when checking w input 
+
+    public static double check() {
+    
+        if (0 < w && w < 1) {
             return 3.5;
-        }
-        else if(1 < w && w <= 3){
+        } else if (1 < w && w <= 3) {
             return 5.5;
-        }
-        else if(3 < w && w <= 10){
+        } else if (3 < w && w <= 10) {
             return 8.5;
-        }
-        else if(10 < w && w <= 20){
+        } else if (10 < w && w <= 20) {
             return 10.5;
-        }
-        else
-        {
+        } else {
             System.out.println("Invalid Input");
             return 0;
-        
+
         }
     }
 }
-
---
+++ 024_ShippingCalculator/tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ShippingCalculator {
+    public static double w;
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input valid weight: ");
+        w = scan.nextDouble();
