11b179006f16c2a55df20522dc10950461f7004a
ThomasRC-bit <54677598+ThomasRC-bit@users.noreply.github.com>
2019-11-05 22:05:39 -0600

    Delete tempCodeRunnerFile.java

--- 024_ShippingCalculator/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ShippingCalculator {
-    public static double w;
-
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Input valid weight: ");
-        w = scan.nextDouble();
