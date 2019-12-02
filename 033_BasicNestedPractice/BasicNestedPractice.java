28ed15036ee8c35ca45027982d39b7652b88b708
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-12-02 09:35:59 -0600

    basic nested practice

--- 033_BasicNestedPractice/BasicNestedPractice.java
+++ 033_BasicNestedPractice/BasicNestedPractice.java
@@ -4,6 +4,7 @@ public class BasicNestedPractice {
     public static void main(String arg[]) {
         Scanner scan = new Scanner(System.in);
 
+        //scans for two numbers and then one character
         System.out.println("Please input a number 1 - 20");
         int row = scan.nextInt();
 
@@ -14,10 +15,13 @@ public class BasicNestedPractice {
         String x = scan.next();
         scan.close();
 
+        //loop that runs as many times for a row
         for (int i = 1; i <= row; i++) {
+            //check to see if it's below 20
             if (row > 20 || column > 20 || x.length() > 1) {
                 break;
             }
+            //nested loop that runs prescribed times, and then again because of the loop above it
             for (int j = 1; j <= column; j++) {
                 System.out.print(x);
             }
