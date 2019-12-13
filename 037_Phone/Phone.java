89d4e2930e51972e73af3d945b9711307f330d90
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-12-13 09:58:43 -0600

    Phone is done

--- 037_Phone/Phone.java
+++ 037_Phone/Phone.java
@@ -1,6 +1,29 @@
-public class Phone{
-    public static void main(String args[])
-    {
-        
+public class Phone {
+
+    public boolean button;
+    public String color;
+    public double version;
+    public String type;
+
+    public Phone(String initType, double initVersion, String initColor, boolean initButton) {
+        button = initButton;
+        color = initColor;
+        version = initVersion;
+        type = initType;
+    }
+    public static void main(String args[]) {
+        //empty
+    }
+    public void isThereAButton() {
+        if (button) {
+            System.out.println("You have a good phone");
+        } else {
+            System.out.println("Bad phone get a better one");
+        }
+    }
+    public void update() {
+        double newVersion = version + 1;
+        System.out.println(version + " updated to " + newVersion);
+
     }
 }
--- 037_Phone/UsingPhone.java
+++ 037_Phone/UsingPhone.java
@@ -1,6 +1,8 @@
 public class UsingPhone{
     public static void main(String args[])
     {
-        
+        Phone p = new Phone("My Phone", 1.0, "Jet Black", true);
+        p.isThereAButton();
+        p.update();
     }
 }
