8be6460da508c20d2328abe2c85899766bebc8e0
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2019-12-18 10:00:01 -0600

    Forgot to save a comment :(

--- 038_BlackjackTwo/Table.java
+++ 038_BlackjackTwo/Table.java
@@ -1,11 +1,11 @@
-public class Table{
-    
-    public static void main(String args[]){
+public class Table {
+
+    public static void main(String args[]) {
+        // creates object from CardDispenser.class
         CardDispenser d = new CardDispenser();
 
-        
+        // runs primary method that plays the game
         d.run();
 
-        // System.out.println(d.test);
     }
 }
