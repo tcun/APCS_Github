149a77f04d9aaf3de812c655d453603711a20167
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2020-01-08 09:54:22 -0600

    Removal
    Cheating command line sorry

--- 038_Pantry/PantryShelf.java
+++
@@ -1,9 +0,0 @@
-public class PantryShelf {
-
-    public static void main(String args[]) {
-        FoodItem f = new FoodItem();
-        // primary run method 
-        f.run();
-        
-    }
-}
