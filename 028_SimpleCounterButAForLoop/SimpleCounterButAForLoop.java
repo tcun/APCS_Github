95e9a25c3eb676d66a1bf4ed48898badb2d2d830
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2020-01-08 10:18:47 -0600

    Assignment Fix
    Oops

--- 028_SimpleCounterButAForLoop/SimpleCounterButAForLoop.java
+++ 028_SimpleCounterButAForLoop/SimpleCounterButAForLoop.java
@@ -1,14 +1,21 @@
 public class SimpleCounterButAForLoop {
 
-    public static void main(String args[])
-    {
-        //literally the same code because i did it both ways the first time
-        for(int i = 0; i < 10; i++){
-        System.out.println(i + 1);
+    public static void main(String args[]) {
+        // literally the same code because i did it both ways the first time
+        for (int i = 0; i < 301; i++) {
+            System.out.println(i + 1);
         }
-        System.out.println();
-        int c = 0;
-        
-    }
 
+        for (int i = 300; i > -1; i--) {
+            System.out.println(i);
+        }
+
+        for (int i = 4798; i < 5325; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 5324; i > 4797; i--) {
+            System.out.println(i);
+        }
+    }
 }
