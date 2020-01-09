96daffe0131780dcb642091313c77121effe4147
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2020-01-09 09:53:46 -0600

    041_PhoneTwo

---
+++ 041_PhoneTwo/Phone.java
@@ -0,0 +1,66 @@
+public class Phone {
+
+    boolean button;
+    String color;
+    double version;
+    String type;
+
+    public Phone(){
+        button = true;
+        color = "Silver";
+        version = 7.2;
+        type = "iPhone 4";
+    }
+
+    public Phone(String initColor) {
+        color = initColor;
+        button = true;
+        version = 7.2;
+        type = "iPhone 4";
+    }
+
+    // Constructor method that intializes variables to avoid static
+    public Phone(String initType, double initVersion, String initColor, boolean initButton) {
+        button = initButton;
+        color = initColor;
+        version = initVersion;
+        type = initType;
+    }
+
+    public static void main(String args[]) {
+        // empty
+    }
+
+    // boolean to check if phone has a button
+    public void isThereAButton() {
+        if (button) {
+            System.out.println("You have a good phone");
+        } else {
+            System.out.println("Bad phone get a better one");
+        }
+    }
+
+    // prints information of object constructed
+    public void update() {
+        double newVersion = version + 1;
+        System.out.println(version + " updated to " + newVersion);
+
+    }
+
+    public boolean getButton() {
+        return button;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getVersion() {
+        return version;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
