ad34fce114b8c44173f05d953997dc0113c69287
Thomas Cunningham <cunningham.thomas.20@c-dh.org>
2020-02-24 10:02:32 -0600

    Comments on 047 & 048

--- 047_RollYourOwnDice/AverageOfDice.java
+++ 047_RollYourOwnDice/AverageOfDice.java
@@ -7,19 +7,20 @@ public class AverageOfDice{
         System.out.println(arrAverage(generateRandomArrayList(cool)));
     }
 
+    // Generating an Integer that is 1-100
     public static Integer hundredSidedDie(){
         double k = Math.random() * 100 + 1;
         Integer wow = new Integer((int)k);
         return wow;
     }
-
+    // Generates a random array with 100 indexes filled with random numbers
     public static ArrayList generateRandomArrayList(ArrayList arr){
         for(int i = 0; i < 100; i++){
             arr.add(hundredSidedDie());
         }
         return arr;
     }
-
+    // Grabbing the average of that random array
     public static int arrAverage(ArrayList arr){
         int total = 0;
         for(int i = 0; i < arr.size(); i++){
--- 048_CleanTempSensorData/CleanTempSensorData.java
+++ 048_CleanTempSensorData/CleanTempSensorData.java
@@ -9,10 +9,11 @@ public class CleanTempSensorData{
         for(int x : nice)
         System.out.println(x);
     }
-
+    // Making function that scans an array and checks bad data then slaps them into an arraylist and returns it
     public static ArrayList cleanData(int[] arr){
         ArrayList<Integer> answer = new ArrayList<>();
         for(int i = 0; i < arr.length; i++){
+            // Checking inputs greater than 15 since those are the bad data points
             if(arr[i] < 15){
                 answer.add(arr[i]);
             }
